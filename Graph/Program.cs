394180edc336ffeddbafa2f26f7cc95377aa7f3b
Bertil Andersen <bertilandersen@hotmail.com>
2020-01-20 19:15:59 +0100

    Add project files.

---
+++ Graph/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instance of our graph
+            MyGraph<string> myGraph = new MyGraph<string>();
+
+            //Create the graph
+            CreateGraph(ref myGraph);
+            Console.ReadKey();
+        }
+
+        private static void CreateGraph(ref MyGraph<string> myGraph)
+        {
+            //Adds nodes to our graph
+            Node<String> n1 = myGraph.AddNode("OREGON");
+            Node<String> n2 = myGraph.AddNode("CALIFORNIA");
+            Node<String> n3 = myGraph.AddNode("IDAHO");
+            Node<String> n4 = myGraph.AddNode("UTAH");
+            Node<String> n5 = myGraph.AddNode("NEW MEXICO");
+            Node<String> n6 = myGraph.AddNode("KANSAS");
+            Node<String> n7 = myGraph.AddNode("SOUTH DAKOTA");
+            Node<String> n8 = myGraph.AddNode("NORTH DAKOTA");
+            Node<String> n9 = myGraph.AddNode("IOWA");
+            Node<String> n10 = myGraph.AddNode("TENNESSEE");
+            Node<String> n11 = myGraph.AddNode("NEW YORK");
+            Node<String> n12 = myGraph.AddNode("FLORIDA");
+            Node<String> n13 = myGraph.AddNode("TEXAS");
+
+            //Creates edges between the graphs nodes
+            myGraph.AddEdge("OREGON", "CALIFORNIA");
+            myGraph.AddEdge("CALIFORNIA", "UTAH");
+            myGraph.AddEdge("UTAH", "IDAHO");
+            myGraph.AddEdge("UTAH", "NEW MEXICO");
+            myGraph.AddEdge("NEW MEXICO", "KANSAS");
+            myGraph.AddEdge("NEW MEXICO", "TEXAS");
+            myGraph.AddEdge("TEXAS", "TENNESSEE");
+            myGraph.AddEdge("TEXAS", "FLORIDA");
+            myGraph.AddEdge("TEXAS", "KANSAS");
+            myGraph.AddEdge("KANSAS", "SOUTH DAKOTA");
+            myGraph.AddEdge("SOUTH DAKOTA", "NORTH DAKOTA");
+            myGraph.AddEdge("NORTH DAKOTA", "IOWA");
+            myGraph.AddEdge("IOWA", "TENNESSEE");
+            myGraph.AddEdge("TENNESSEE", "FLORIDA");
+            myGraph.AddEdge("TENNESSEE", "NEW YORK");
+        }
+    }
+}
